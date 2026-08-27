432432230379bbd935d55515fcdc465825d6abc3
Isabell <isabellviolaamanda@gmail.com>
2026-08-27 13:09:18 +0200

    Cleaned up project

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
 
-const app = mount(App, {
-  target: document.getElementById('app'),
-})
+const target = document.getElementById('app')
+
+// Kontrollera om HTML elementet finns innan appen startas
+if (!target) {
+  throw new Error('No target element found')
+}
+
+// Starta appen om elementet finns
+const app = mount(App, { target })
 
 export default app
